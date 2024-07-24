277171bea4fd793f7e281e1acee46a51b76060d2
MevWebDev <szymongrysiewicz@gmail.com>
2024-07-24 15:41:13 +0200

    Refactor dropdown menu event listeners for better performance

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const dropdown = document.querySelector(".dropdown");
 const dropdownContent = document.querySelector(".dropdown-content");
-console.log(dropdownContent);
+
 dropdown.addEventListener("mouseover", () => {
   dropdownContent.style.display = "flex";
 });
-dropdown.addEventListener("mouseout", () => {
+dropdownContent.addEventListener("mouseout", () => {
   dropdownContent.style.display = "none";
 });
