22be805dca0f2188c2c5e55395900dbd025d444f
MevWebDev <szymongrysiewicz@gmail.com>
2024-07-24 15:08:46 +0200

    feat: Add dropdown functionality to navigation menu

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var franek = "franek";
+const dropdown = document.querySelector(".dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+console.log(dropdownContent);
+dropdown.addEventListener("mouseover", () => {
+  dropdownContent.style.display = "flex";
+});
+dropdown.addEventListener("mouseout", () => {
+  dropdownContent.style.display = "none";
+});
